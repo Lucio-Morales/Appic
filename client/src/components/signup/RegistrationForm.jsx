f136ed9f16efea5938429be8afc1aa5534eb2a75
Falekin7 <falekinldg@gmail.com>
2024-05-30 21:44:37 -0300

    start user registration func

---
+++ client/src/components/signup/RegistrationForm.jsx
@@ -0,0 +1,19 @@
+
+const RegistrationForm = () => {
+    return (
+        <>
+            <form>
+                <label htmlFor="">Name</label>
+                <input type="text" />
+                <label htmlFor="">Email</label>
+                <input type="text" />
+                <label htmlFor="">Password</label>
+                <input type="password" />
+
+            </form>
+        </>
+
+    )
+}
+
+export default RegistrationForm
